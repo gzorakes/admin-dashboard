1584b7b7d9c3fa4e59bd0a15c192aa4c38e09571
zorakisgeorge <zorakisgeorge@gmail.com>
2025-09-21 13:32:53 +0300

    Products & Users page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import OverviewPage from "./pages/OverviewPage";
 import ProductsPage from "./pages/ProductsPage";
 
 import Sidebar from "./components/common/Sidebar";
+import UsersPage from "./pages/UsersPage";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
       <Routes>
         <Route path="/" element={<OverviewPage />} />
         <Route path="/products" element={<ProductsPage />} />
+        <Route path="/users" element={<UsersPage />} />
       </Routes>
     </div>
   );
